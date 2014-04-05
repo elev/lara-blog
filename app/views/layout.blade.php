968c6b7a814f9e0b5241b4425a3c453a116f2fe7
Lucas Boyd <LucasBoydDev@gmail.com>
2014-04-05 13:25:01 -0400

    added twitter bootstrap and jquery

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,9 +1,13 @@
 <html>
 	<head>
+		{{ HTML::style('packages/bootstrap-3.1.1-dist/css/bootstrap.css') }}
 	</head>
 	<body>
 		<h1>Laravel Quickstart</h1>
 
 		@yield('content')
+
+		{{ HTML::script('http://code.jquery.com/jquery-1.10.1.min.js') }}
+		{{ HTML::script('packages/bootstrap-3.1.1-dist/js/bootstrap.js') }}
 	</body>
 </html>
