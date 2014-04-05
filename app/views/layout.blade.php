75d278b54a479636ab68d6b26033fe4db1cd71f5
Lucas Boyd <LucasBoydDev@gmail.com>
2014-04-05 15:15:57 -0400

    edited the main container layout

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,8 +3,16 @@
 		{{ HTML::style('packages/bootstrap-3.1.1-dist/css/bootstrap.css') }}
 	</head>
 	<body>
-		<h1>Laravel Quickstart</h1>
-
+		<div class="jumbotron">
+		      <div class="container">
+		        <h1>Laravel Test Blog</h1>
+		        <p>This is a test for Laravel and Bootstrap.
+		        	We are using bootstrap for minimal modern styles and 
+		        	using laravel for data abstraction, authentication, and routing.
+		        </p>
+<!-- 		        <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
+ -->		      </div>
+		    </div>
 		@yield('content')
 
 		{{ HTML::script('http://code.jquery.com/jquery-1.10.1.min.js') }}
