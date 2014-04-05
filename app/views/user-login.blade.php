e93dd0a04dbad9971cad96591e62162c89523da8
Lucas Boyd <LucasBoydDev@gmail.com>
2014-04-05 14:19:49 -0400

    styled login form

--- app/views/user-login.blade.php
+++ app/views/user-login.blade.php
@@ -1,9 +1,29 @@
 @extends('layout')
 
 @section('content')
-	{{ Form::open(array('url' => 'login-check')) }}
-		<div class="block">	{{ Form::label('email', 'Email') }} {{ Form::text('email') }}</div>
-		<div class="block">	{{ Form::label('password', 'Password') }} {{ Form::password('password') }}</div>
-		<div class="block">	{{ Form::submit('submit') }}</div>
-	{{ Form::close() }}
+	<div class="container">
+		{{ Form::open(array('url' => 'login-check', 'class' => 'form-horizontal')) }}
+
+			<div class="form-group">
+				{{ Form::label('email', 'Email', array('class' => 'col-sm-2 control-label')) }}
+				<div class="col-sm-5">
+					{{ Form::text('email', null, array('class' => 'form-control', 'placeholder' => 'email@example.com')) }}
+				</div><!--/.col-sm-5 -->
+			</div><!--/.form-group-->
+
+			  <div class="form-group">
+			  	{{ Form::label('password', 'Password', array('class' => 'col-sm-2 control-label')) }} 
+			    <div class="col-sm-5">
+			    	{{ Form::password('password',  array('class' => 'form-control', 'placeholder' => 'password')) }}
+			    </div><!--/.col-sm-5 -->
+			  </div><!--/.form-group-->
+
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-5">
+					{{ Form::submit('submit', array('class' => 'btn btn-primary')) }}
+				</div> <!--/.col-sm-5 -->
+			</div><!--/.form-group-->
+
+		{{ Form::close() }}
+	</div><!--/.container -->
 @stop
