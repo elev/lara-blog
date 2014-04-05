f8e715300657fa721b0bda6705a87fc715041bf5
Lucas Boyd <LucasBoydDev@gmail.com>
2014-04-05 15:09:34 -0400

    user page styles updated

--- app/views/user-profile.blade.php
+++ app/views/user-profile.blade.php
@@ -1,6 +1,12 @@
 @extends('layout')
 
 @section('content')
-	<p>{{$user->name}}</p>
-	<p>{{$user->email}}</p>
+	<div class="container">	
+		<h3>User Information</h3>
+		<ul class="list-group col-sm-5">
+			<li class="list-group-item"><span class="col-sm-3">Name:</span>{{$user->name}}</li>
+			<li class="list-group-item"><span class="col-sm-3">Email:</span>{{$user->email}}</li>
+			<li class="list-group-item"><span class="col-sm-3">Created:</span>{{$user->created_at}}</li>
+		</ul>
+	</div> <!-- ./container-->
 @stop
--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -1,6 +1,8 @@
 @extends('layout');
 
 @section('content');
+	<div class="container">
 	<p>{{$user->name}}</p>
 	<p>{{$user->email}}</p>
+	</div><!--/.container-->
 @stop
