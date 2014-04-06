052a1f7a585282fa1344b4cf13dd2ed6b83292bf
Lucas Boyd <LucasBoydDev@gmail.com>
2014-04-06 16:50:26 -0400

    reformat blog create page

--- app/views/blog-create.blade.php
+++ app/views/blog-create.blade.php
@@ -1,9 +1,26 @@
 @extends('layout')
 
 @section('content')
-	{{ Form::open(array('action' => 'BlogController@processNew')) }}
-	<div class="block">	{{ Form::label('name', 'Name') }} {{ Form::text('name') }}</div>
-	<div class="block">	{{ Form::label('content', 'Content') }} {{ Form::textarea('content') }}</div>
-	<div class="block">	{{ Form::submit('submit') }}</div>
-	{{ Form::close() }}
+	<div class="container">
+		<h2>Create A New Blog Entry</h2>
+		{{ Form::open(array('action' => 'BlogController@processNew', 'class' => 'form-horizontal')) }}
+		<div class="form-group">
+			{{ Form::label('name', 'Name', array('class' =>'col-sm-2 control-label')) }}
+			<div class="col-sm-5">
+				{{ Form::text('name', null, array('class' => 'form-control', 'placeholder' => 'Name')) }}
+			</div>
+		</div>
+		<div class="form-group">
+			{{ Form::label('content', 'Content', array('class'=>'col-sm-2 control-label')) }}
+			<div class="col-sm-5">	
+				{{ Form::textarea('content', null, array('class' => 'form-control', 'placeholder' => 'Content')) }}
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-5">
+				{{ Form::submit('submit', array('class'=> 'btn btn-primary')) }}
+			</div>
+		</div>
+		{{ Form::close() }}
+	</div><!--/.container-->
 @stop
