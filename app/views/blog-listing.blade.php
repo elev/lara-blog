23831c7f0a03291d846063f7b11250ee66733d11
Lucas Boyd <LucasBoydDev@gmail.com>
2014-04-06 16:13:58 -0400

    reformat blog listing

--- app/views/blog-listing.blade.php
+++ app/views/blog-listing.blade.php
@@ -1,9 +1,24 @@
 @extends('layout')
 
 @section('content')
-	@foreach($blogs as $blog)
-		<p>{{$blog->name}}</p>
-		<p>{{$blog->content}}</p>
-		<hr>
-	@endforeach
+	<div class="container">
+		
+		<div class="blog-header">
+			<h2 class="blog-title">Blog Entries</h2>
+			<p class="lead blog-description">These are blog listings</p>
+		</div> <!--/.blog-header-->
+		<div class="row">
+			<div class="col-sm-8 blog-main">
+
+				@foreach($blogs as $blog)
+					<div class="blog-post">
+						<h3 class="blog-post-title">{{$blog->name}}</h3>
+						<p>{{$blog->content}}</p>
+						<hr>
+					</div>
+				@endforeach
+
+			</div><!--/.blog-main-->
+		</div><!--/.row-->
+	</div><!--/.container-->
 @stop
